c581910c463bac5803a59946e07f0c0cf7b9d03d
Amanda Lopes <amandaltoliveira@gmail.com>
2025-08-04 12:19:29 -0300

    adicionando sortearAmigo

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 let listaDeNomes = [];
 
 function adicionarAmigo() {
-    let nome = document.querySelector('#amigo').value;
+    let nome = document.querySelector('#amigo').value.trim();
 
     if (nome === '') {
-        alert('Preencha o campo de adicionar um nome!');
+        alert('Preencha o campo de adicionar nome!');
     } else {
         listaDeNomes.push(nome);
+        atualizarListaAmigos(); 
+        
+        limparCampo();
     }
-
-    atualizarListaAmigos(); 
-    limparCampo();
 }
+
+
 
 function limparCampo() {
     document.querySelector('#amigo').value = '';
@@ -24,4 +26,16 @@ function atualizarListaAmigos() {
     for (let i = 0; i < listaDeNomes.length; i++) {
         lista.innerHTML += `<li>${listaDeNomes[i]}</li>`;
     }
+}
+
+function sortearAmigo() {
+    if (listaDeNomes.length < 2) {
+        alert('Adicione pelo menos 2 nomes no sorteio!');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * listaDeNomes.length);
+    let nomeSorteado = listaDeNomes[indiceAleatorio];
+
+    document.getElementById('resultado').innerHTML = `Nome sorteado: ${nomeSorteado}`;
 }
